b8cd665c698916cb17a4c10219dd53b999560273
Ernesto Vázquez <vazquezgarciaernesto@gmail.com>
2020-06-17 12:22:31 +0200

    cambios profile

--- 2-primertemplate/1-primerrecurso/main.tf
+++ 2-primertemplate/1-primerrecurso/main.tf
@@ -10,10 +10,9 @@ provider "aws" {
 
 resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = false
-  enable_dns_support = false
+  enable_dns_hostnames = true
+  enable_dns_support = true
   tags = {
     Name = "proyectoernesto"
   }
 }
-
