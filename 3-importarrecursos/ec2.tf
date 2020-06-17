344b2bc16fab122cfff5ec4b4b7f5c096c265124
Ernesto Vázquez <vazquezgarciaernesto@gmail.com>
2020-06-17 12:28:05 +0200

    profile tema 3

--- 3-importarrecursos/ec2.tf
+++ 3-importarrecursos/ec2.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "web" {
   ami = "ami-034d940df32c75d15"
   instance_type = "t2.small"
   tags = {
-    Name = "proyectoterra-webserver"
+    Name = "proyectoernesto-webserver"
   }
 }
